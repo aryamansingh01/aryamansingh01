a2f1553ac4d2c4040677c207fec898b9f117446c
aryamansingh01 <71206727+aryamansingh01@users.noreply.github.com>
2025-01-21 11:39:30 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -195,7 +195,7 @@ function App() {
                   "Applied advanced statistical analyses to evaluate platform optimizations, projecting a 10% increase in user engagement."
                   "Implemented personalized recommendation engines, enhancing customer experience and driving higher conversion rates."
                   "Presented key findings to cross-functional teams, enabling data-driven strategies that improved ROI by 10%."
-                ]{
+                ]}{
                 title: "Data Analyst Intern",
                 company: "Hermes Infotech",
                 date: "May 2022 – December 2022 ",
@@ -204,7 +204,7 @@ function App() {
                   "Aggregated and refined large e-commerce datasets using Python and SQL, resolving data irregularities and cutting manual data management by 20%."
                   "Designed interactive dashboards with Tableau and Power BI, streamlining real-time reporting and facilitating quicker decision-making for stakeholders."
                   "Executed A/B testing to pinpoint user engagement factors, steering product refinements and raising user satisfaction."
-                ]{
+                ]}{
                 title: "Data Analyst Intern",
                 company: "NeoDocto",
                 date: "Febuary 2022 – April 2022 ",
@@ -212,7 +212,7 @@ function App() {
                 description: [
                   "Managed full-cycle data analysis projects, using Python and SQL to successfully deliver insights supporting acquisition and retention strategy improvements",
                   "Collaborated with product and marketing teams to deploy data-driven solutions, improving cross-team communication and project execution"
-                ]
+                ]}
               },
               
             ].map((exp, i) => (
